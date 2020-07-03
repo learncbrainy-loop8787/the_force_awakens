019a359e86858eb4dd6153ebda19059097872d75
learncbrainy-loop8787 <60646147+learncbrainy-loop8787@users.noreply.github.com>
2020-07-03 10:22:27 -0700

    Add files via upload

---
+++ the_force/lib/the_force/environment.rb
@@ -0,0 +1,8 @@
+require "pry"
+require 'rest-client'
+require 'json'
+
+require_relative "./the_force/version"
+require_relative "./the_force/api.rb"
+require_relative "./the_force/cli.rb"
+require_relative "./the_force/planets.rb"
